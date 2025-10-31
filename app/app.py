aa8e7b84829d6bd5dd720a493693d6a40f28062d
Kashefy <kashefymajid1992@gmail.com>
2025-10-31 15:47:00 +0330

    update app

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello from Flask instance!  ID: {random.randint(1000,9999)}"
+    return f"Hello from Flask new :) instance!  ID: {random.randint(1000,9999)}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
