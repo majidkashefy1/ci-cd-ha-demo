a1f8aed8797154523fdb6ba49fb413277b18fcc3
Kashefy <kashefymajid1992@gmail.com>
2025-10-31 16:07:34 +0330

    add monitoring

--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from prometheus_client import start_http_server, Counter
 import random
 
 app = Flask(__name__)
+REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests')
 
 @app.route('/')
 def home():
-    return f"Hello from Flask new :) instance!  ID: {random.randint(1000,9999)}"
+    REQUEST_COUNT.inc()
+    return f"Hello from Flask instance. ID: {random.randint(1000,9999)}"
 
 if __name__ == '__main__':
+    start_http_server(8000)  #
     app.run(host='0.0.0.0', port=5000)
