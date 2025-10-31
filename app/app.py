4976c36974c64d0de48fc36dec58ba993a63c488
Kashefy <kashefymajid1992@gmail.com>
2025-10-31 15:11:48 +0330

    ready for pull

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"Hello from Flask instance!  ID: {random.randint(1000,9999)}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
